dc2e37666d67ad349e3d010794fe3f035711e9e0
Paolo <paolo.ronchese@cern.ch>
2014-01-23 10:29:49 +0100

    environment set scripts improved

--- uty/envsetTemplate.csh
+++ uty/envsetTemplate.csh
@@ -1,7 +1,8 @@
 setenv NTU_TOOL_DIR INSTALLDIR
-setenv LD_LIBRARY_PATH `printenv | grep LD_LIBRARY_PATH | awk -F= '{print $2}'`
+setenv LD_LIBRARY_PATH `printenv | grep LD_LIBRARY_PATH | awk -F= '($1=="LD_LIBRARY_PATH"){print $2}'`
 eval `echo $LD_LIBRARY_PATH | awk -F: '{for(i=1;i<=NF;++i)print $i}' | grep -v NtuTool | awk 'BEGIN{printf("setenv LD_LIBRARY_PATH ")} {printf($0":")} END{print " ;"}'`
 setenv LD_LIBRARY_PATH ${LD_LIBRARY_PATH}${NTU_TOOL_DIR}/lib
 if ( $?SCRAM_ARCH ) then
-setenv LD_LIBRARY_PATH ${LD_LIBRARY_PATH}:${NTU_TOOL_DIR}/lib/${SCRAM_ARCH}
+setenv LD_LIBRARY_PATH ${LD_LIBRARY_PATH}":"
+setenv LD_LIBRARY_PATH ${LD_LIBRARY_PATH}${NTU_TOOL_DIR}/lib/${SCRAM_ARCH}
 endif
--- uty/envsetTemplate.sh
+++ uty/envsetTemplate.sh
@@ -1,7 +1,8 @@
 export NTU_TOOL_DIR=INSTALLDIR
-export LD_LIBRARY_PATH=`printenv | grep LD_LIBRARY_PATH | awk -F= '{print $2}'`
+export LD_LIBRARY_PATH=`printenv | grep LD_LIBRARY_PATH | awk -F= '($1=="LD_LIBRARY_PATH"){print $2}'`
 eval `echo $LD_LIBRARY_PATH | awk -F: '{for(i=1;i<=NF;++i)print $i}' | grep -v NtuTool | awk 'BEGIN{printf("export LD_LIBRARY_PATH=")} {printf($0":")} END{print " ;"}'`
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}${NTU_TOOL_DIR}/lib
 if [ -n "${SCRAM_ARCH}" ]; then
-export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${NTU_TOOL_DIR}/lib/${SCRAM_ARCH}
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}":"
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}${NTU_TOOL_DIR}/lib/${SCRAM_ARCH}
 fi
