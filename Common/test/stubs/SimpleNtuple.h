da0552b5c80ad8353aa82d4322bf8be6fd31eb34
Paolo <paolo.ronchese@cern.ch>
2014-07-02 17:11:12 +0200

    examples updated

--- Common/test/stubs/SimpleNtuple.h
+++ Common/test/stubs/SimpleNtuple.h
@@ -19,20 +19,21 @@ class SimpleNtuple: public virtual TreeWrapper {
   // Declaration of leaf types
 
   int i_run;                    // a number
-  std::vector<int>    i_vec;    // a vector (in the stack)
-  std::vector<float>* f_vpt;    // a vector (in the heap)
 
   int  n_max;
   int  n_arr;
   int* i_arr;                   // an array with "n_arr" elements
 
+  std::vector<int>    i_vec;    // a vector (in the stack)
+  std::vector<float>* f_vpt;    // a vector (in the heap)
+
   // List of branches
 
   TBranch* b_i_run;
-  TBranch* b_i_vec;
-  TBranch* b_f_vpt;
   TBranch* b_n_arr;
   TBranch* b_i_arr;
+  TBranch* b_i_vec;
+  TBranch* b_f_vpt;
 
  private:
 
