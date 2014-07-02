20f5ed0c00a693f3c0dcd485cf13f099ddd3874e
Paolo <paolo.ronchese@cern.ch>
2014-07-02 17:50:34 +0200

    examples updated

---
+++ EDM/test/stubs/SimpleEDMWriter.h
@@ -0,0 +1,45 @@
+#ifndef TestBaseNtuple_h
+#define TestBaseNtuple_h
+
+#include "NtuTool/Common/test/stubs/SimpleNtuple.h"
+#include "NtuTool/EDM/interface/EDMTreeWriter.h"
+
+#include "FWCore/Framework/interface/EDAnalyzer.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/EventSetup.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+
+class TFile;
+
+// to produce EDM ntuples the class must inherit from "EDMTreeWriter"
+// "EDMTreeWriter" in turn inherits from EDProducer
+// the ntuple definition is exactly in the same class "SimpleNtuple"
+
+class SimpleEDMWriter: public SimpleNtuple,
+                       public EDMTreeWriter {
+
+ public:
+
+  explicit SimpleEDMWriter( const edm::ParameterSet& ps );
+  virtual ~SimpleEDMWriter();
+
+  virtual void beginJob();
+  virtual void endJob();
+
+  // the function
+  //     void produce( edm::Event& ev, const edm::EventSetup& es );
+  // is provided by the tool
+
+ private:
+
+  // the user code contained in the "analyze" function when 
+  // writing standard trees in an EDAnalyzer is moved to 
+  // a "fill" function called by the library
+  virtual bool fill( const edm::Event& ev, const edm::EventSetup& es );
+
+  // function (by the user) to encapsulate operations to set variables
+  void setData( unsigned int iRun );
+
+};
+
+#endif
---
+++ EDM/test/stubs/SimpleWriter.h
@@ -0,0 +1,43 @@
+#ifndef TestBaseNtuple_h
+#define TestBaseNtuple_h
+
+#include "NtuTool/Common/test/stubs/SimpleNtuple.h"
+#include "NtuTool/Common/interface/TreeWriter.h"
+
+#include "FWCore/Framework/interface/EDAnalyzer.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/EventSetup.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+
+class TFile;
+
+// the analyzer inherits from two more classes in addition to EDAnalyzer:
+//     "SimpleTree"   is defined from the user and contains the variables
+//     "TreeWriter"   is defined inside the tool
+
+class SimpleWriter: public edm::EDAnalyzer,
+                    public SimpleNtuple,
+                    public TreeWriter {
+
+ public:
+
+  explicit SimpleWriter( const edm::ParameterSet& ps );
+  virtual ~SimpleWriter();
+
+  virtual void beginJob();
+  virtual void analyze( const edm::Event& ev, const edm::EventSetup& es );
+  virtual void endJob();
+
+ private:
+
+  TFile* file;
+
+  // the pointer to the TTree is no more needed
+  // the variables are now definied inside "SimpleTree"
+
+  // function (by the user) to encapsulate operations to set variables
+  void setData( unsigned int iRun );
+
+};
+
+#endif
