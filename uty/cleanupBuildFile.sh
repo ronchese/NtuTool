1523c5f9d68223f8eb70e873d6a3ccf2a3f42ad2
Paolo <paolo.ronchese@cern.ch>
2015-12-08 14:58:30 +0100

    BuildFile cleanup

--- uty/cleanupBuildFile.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-export FILE=$1
-export FTMP=/tmp/`basename ${FILE}``date +%s`
-
-awk '(index($0,"rootcintex")==0){print $0}' ${FILE} > ${FTMP}
-cp ${FTMP} ${FILE}
-rm ${FTMP}
--- uty/updateTo7.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-export DIR=`dirname $0`
-export TOP=`dirname ${DIR}`
-
-eval `find ${TOP} | grep BuildFile | grep 'xml$' | awk -v DIR=${DIR} '{print DIR"/cleanupBuildFile.sh "$0" ;"}'`
