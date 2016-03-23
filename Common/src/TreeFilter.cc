7874998fd921177d9069106dd596506be8b5cea5
Paolo <paolo.ronchese@cern.ch>
2016-03-23 11:20:23 +0100

    wildcards in TreeFilter::dropBranch added

--- Common/src/TreeFilter.cc
+++ Common/src/TreeFilter.cc
@@ -6,6 +6,8 @@
 #include "TFile.h"
 #include "TDirectory.h"
 
+#include <string>
+#include <vector>
 
 TreeFilter::TreeFilter() {
   fhManager = new TreeTypeManager;
@@ -17,15 +19,60 @@ TreeFilter::~TreeFilter() {
 
 
 void TreeFilter::dropBranch( const std::string& name ) {
-  skimDrop.insert( name );
+  if ( name.find( "*" ) == std::string::npos ) {
+    skimDrop.insert( name );
+  }
+  else {
+    std::vector<std::string> sl;
+    sl.reserve( 100 );
+    std::size_t p = 0;
+    std::size_t n = 0;
+    while ( ( n = name.find( "*", p ) ) != std::string::npos ) {
+      sl.push_back( name.substr( p, n - p ) );
+      p = n + 1;
+    }
+    sl.push_back( name.substr( p ) );
+    bool bb = sl.front().length();
+    bool eb = sl.back ().length();
+    branch_iterator iter = treeBegin();
+    branch_iterator iend = treeEnd();
+    while ( iter != iend ) {
+      branch_desc* bDesc = *iter++;
+      const std::string& bn = *bDesc->branchName;
+      n = p = 0;
+      bool match = true;
+      int i;
+      int l = sl.size();
+      for ( i = 0; i < l; ++i ) {
+        const std::string& dn = sl[i];
+        if ( dn.length() == 0 ) continue;
+        if ( ( n = bn.find( dn, p ) ) == std::string::npos ) {
+          match = false;
+          break;
+        }
+	if ( bb && ( i ==   0       )
+                && ( n != 0 ) ) {
+          match = false;
+          break;
+        }
+	if ( eb && ( i == ( l - 1 ) )
+                && ( n != ( bn.length() - dn.length() ) ) ) {
+          match = false;
+          break;
+        }
+        p = n + dn.length();
+      }
+      if ( match ) skimDrop.insert( bn );
+    }
+  }
   return;
 }
 
 
 void TreeFilter::initWrite( TFile* file ) {
 
-  TDirectory* currentDir = gDirectory;
   if ( file != 0 ) {
+    TDirectory* currentDir = gDirectory;
     file->cd();
     std::string fullName = treeName;
     int dirNameLength;
@@ -39,11 +86,12 @@ void TreeFilter::initWrite( TFile* file ) {
     const char* name = fullName.c_str();
     treeDir = gDirectory;
     filterTree = new TTree( name, name );
+    currentDir->cd();
   }
   else {
     filterTree = 0;
+    return;
   }
-  currentDir->cd();
 
   branch_iterator iter = treeBegin();
   branch_iterator iend = treeEnd();
@@ -63,13 +111,12 @@ void TreeFilter::initWrite( TFile* file ) {
                                                      fhManager );
     if ( bDesc->ppRef ) dataPtr =  reinterpret_cast<void**>( bDesc->dataPtr );
     else                dataPtr = &bDesc->dataPtr;
-    if ( file != 0 ) {
-      if ( bDesc->splitLevel < 0 )
-           filterTree->Branch( bDesc->branchName->c_str(),
-                                    bDesc->dataPtr,
-                                    bDesc->branchData->c_str() );
-      else handler->branch( filterTree, bDesc->branchName, dataPtr,
-                                bDesc->bufferSize, bDesc->splitLevel );
+    if ( bDesc->splitLevel < 0 )
+         filterTree->Branch( bDesc->branchName->c_str(),
+                             bDesc->dataPtr,
+                             bDesc->branchData->c_str() );
+    else handler->branch( filterTree, bDesc->branchName, dataPtr,
+                                      bDesc->bufferSize, bDesc->splitLevel );
 //      if ( bDesc->splitLevel < 0 )
 //           b = currentTree->Branch( bDesc->branchName->c_str(),
 //                                    bDesc->dataPtr,
@@ -77,7 +124,6 @@ void TreeFilter::initWrite( TFile* file ) {
 //      else b = handler->branch( currentTree, bDesc->branchName, dataPtr,
 //                                bDesc->bufferSize, bDesc->splitLevel );
 //      if ( bDesc->branchPtr != 0 ) *bDesc->branchPtr = b;
-    }
   }
 
   return;
