fb53210999ab472a40701f56af4df1074c0901f5
Paolo <paolo.ronchese@cern.ch>
2021-06-08 11:36:34 +0200

    small fix in compile script

--- uty/compile.sh
+++ uty/compile.sh
@@ -3,9 +3,8 @@
 cd `dirname $0`
 cd ..
 export NTU_TOOL_DIR=`/bin/pwd`
-#cd ${NTU_TOOL_DIR}
 
-rm -f lib/libNtupleTool.so
+rm -f ${NTU_TOOL_DIR}/lib/libNtupleTool.so
 c++ -I ${NTU_TOOL_DIR}/include `root-config --cflags` -fPIC \
-    --shared -o $NTU_TOOL_DIR/lib/libNtupleTool.so \
+    --shared -o ${NTU_TOOL_DIR}/lib/libNtupleTool.so \
     Common/src/*.cc  Read/src/*.cc
